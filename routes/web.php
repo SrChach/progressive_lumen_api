a2755af93f5e7e8b533e33fab7ee99837890c8be
srchach <gsikmo@gmail.com>
2019-05-13 17:05:08 -0500

    Ejemplo de JSON para PDFmake

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-// Falta un middleware que solo cheque si ya estás dentro o no, sin importar que roles tengas (cualquiera registrado puede tener una firma) 
+// Falta un middleware que solo cheque si ya estás dentro o no, sin importar que roles tengas (cualquiera registrado puede tener una firma)
+
+// faltan las funciones para responder automático errores y success, sin necesidad de tanto codigo
 
 $router->get('/', function () use ($router) {
 	return "RESTful API under build";
@@ -40,5 +42,10 @@ $router->get('/file', function(){
 	$json = file_get_contents("../storage/app/template.json");
 	$replacers = ['palabra_cambiada_1', 'palabra_cambiada_2', 'palabra_cambiada_3'];
 	$json = reemplazar_uno_con_varios('codified_content', $replacers, $json);
+	return response($json)->header('Content-Type', 'application/json');
+});
+
+$router->get('/plantilla', function(){
+	$json = file_get_contents("../storage/app/plantilla.json");
 	return response($json)->header('Content-Type', 'application/json');
 });

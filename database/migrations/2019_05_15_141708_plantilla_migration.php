18238737731e7e31da6c36094d45a27217baed62
srchach <gsikmo@gmail.com>
2019-05-15 09:26:06 -0500

    Added template logic, beginning to configure it

---
+++ database/migrations/2019_05_15_141708_plantilla_migration.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PlantillaMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plantillas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('archivo')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plantillas');
+    }
+}

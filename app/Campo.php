fadbcc4e51d985ebd2d775736b18307800b0406d
srchach <gsikmo@gmail.com>
2019-05-15 18:46:28 -0500

    Modelos para el manejo de documentos en BD

---
+++ app/Campo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class Campo extends Model implements AuthenticatableContract, AuthorizableContract
+{
+	use Authenticatable, Authorizable;
+
+	protected $table = 'plantillas';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'id', 'palabraClave', 'vecesRepite', 'isImage'
+	];
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'id'
+	];
+	
+	/*
+	public function Campos(){
+		return $this->hasMany('App\Campo');
+	}
+	*/
+}
